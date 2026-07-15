75fba437d093380e41c2d1eb75977db38bb6be51
dynatrace-asad-ali <asad.ali@dynatrace.com>
2026-07-15 11:34:21 -0700

    Removed unnecessary env variables. Also added logic to get Azure related stuff from secret server

--- .devcontainer/deployment.sh
+++ .devcontainer/deployment.sh
@@ -1,9 +1,26 @@
+SECRET_SERVER="http://52.186.168.229:3800"
+
+fetch_secret() {
+  curl -s -X POST "$SECRET_SERVER/$1" \
+    -H "Content-Type: application/json" \
+    -d "{\"password\": \"$SECRET_SERVER_PASSWORD\"}"
+}
+
+# Fetch Azure and OTel values from secret server
+AZURE_ENDPOINT=$(fetch_secret azure_endpoint)
+AZURE_DEPLOYMENT=$(fetch_secret azure_deployment)
+AZURE_SUBSCRIPTION_KEY=$(fetch_secret azure_subscription_key)
+AZURE_API_VERSION=$(fetch_secret azure_api_version)
+OTEL_SERVICE_NAME=$(fetch_secret otel_service_name)
+
+# Replace placeholders in setEnv.sh
 sed -i "s,DYNATRACE_EXPORTER_OTLP_ENDPOINT_TOREPLACE,$DYNATRACE_EXPORTER_OTLP_ENDPOINT," /workspaces/$RepositoryName/setEnv.sh
 sed -i "s,DYNATRACE_API_TOKEN_TOREPLACE,$DYNATRACE_API_TOKEN," /workspaces/$RepositoryName/setEnv.sh
 sed -i "s,AZURE_ENDPOINT_TOREPLACE,$AZURE_ENDPOINT," /workspaces/$RepositoryName/setEnv.sh
 sed -i "s,AZURE_DEPLOYMENT_TOREPLACE,$AZURE_DEPLOYMENT," /workspaces/$RepositoryName/setEnv.sh
 sed -i "s,AZURE_SUBSCRIPTION_KEY_TOREPLACE,$AZURE_SUBSCRIPTION_KEY," /workspaces/$RepositoryName/setEnv.sh
 sed -i "s,AZURE_API_VERSION_TOREPLACE,$AZURE_API_VERSION," /workspaces/$RepositoryName/setEnv.sh
+sed -i "s,OTEL_SERVICE_NAME_TOREPLACE,$OTEL_SERVICE_NAME," /workspaces/$RepositoryName/setEnv.sh
 sed -i "s,TAVILY_API_KEY_TOREPLACE,$TAVILY_API_KEY," /workspaces/$RepositoryName/setEnv.sh
 
 source /workspaces/$RepositoryName/setEnv.sh
